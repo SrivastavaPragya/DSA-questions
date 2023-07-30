4ab4d255f903d8b93d795d2f839b9ea87cdfd1f6
Pragya Srivastava <126488693+SrivastavaPragya@users.noreply.github.com>
2023-07-30 09:27:22 +0530

    Add files via upload

---
+++ LinkdeList4.cpp
@@ -0,0 +1,29 @@
+// 10 6 77 90 61 67 100 -1
+//4
+//90 61 67 100 10 6 7
+
+#include<iostream>
+
+using namespace std;
+Node *appendLastNToFirst(Node *head, int n)
+{
+   cout=0;
+   Node*temp2=head;
+   Node*temp=head;
+   while(temp!=nullptr&&count<n-1){
+    temp=temp->next;
+    count++
+   }
+   if(temp!=nullptr){
+    Node*a=temp->next;
+    Node*a=head;
+   }
+   Node*temp3=head
+   while(temp3!=nullptr){
+    temp3=temp3->next;
+   }
+   if(temp3==nullptr){
+    temp3->next=temp2;
+   }
+   return head
+}
