1db7234d2b169ac198bf3a66dd2265e0561c4927
Pragya Srivastava <126488693+SrivastavaPragya@users.noreply.github.com>
2023-08-23 23:27:03 +0530

    Add files via upload

---
+++ BinaryTree4.cpp
@@ -0,0 +1,11 @@
+// preorder traversal
+
+void preOrder(BinaryTreeNode<int> *root) {
+	// Write your code here
+	if(root==NULL)
+	return;
+
+	cout<<root->data<<" ";
+	preOrder(root->left);
+	preOrder(root->right);
+}
