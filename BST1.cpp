913552330da765867f8da543b93f7d5799a922e0
Pragya Srivastava <126488693+SrivastavaPragya@users.noreply.github.com>
2023-08-26 22:44:56 +0530

    Add files via upload

---
+++ BST1.cpp
@@ -0,0 +1,28 @@
+// searching in bst
+bool searchInBST(BinaryTreeNode<int> *root , int k) {
+	// Write your code here
+if (root==NULL){
+	return false;
+}
+if(root->data==k){
+	return true;
+}
+
+if(k>root->data){
+	bool rightT=searchInBST(root->right , k);
+	if(rightT==true)
+	{
+		return true;
+	}
+
+}
+if(k<root->data){
+		bool leftT=searchInBST(root->left  , k);
+		if(leftT==true)
+		{
+			return true;
+		}
+	
+}
+return false;
+}
